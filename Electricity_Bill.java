2935ebc9b62e27e8e879dc6f64fc9bd93a1e0f93
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-01 22:50:03 +0530

    import java.util.Scanner; class sample { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int a; float b; String Custmerid,name; Custmerid=Sc.nextLine(); name=Sc.nextLine(); a=Sc.nextInt(); if(a<=199) { b=a*1.20f; } else if(a>=200 && a<400) { b=a*1.50f; } else if (a>=400 && a<600) { b=a*1.80f; } else b=a*2.00f; if(b>400) { b=b*0.15f+b; } else b=b+100; System.out.format("%.2f",b); }}

---
+++ Electricity_Bill.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class sample
+{ public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int a; float b; String Custmerid,name; Custmerid=Sc.nextLine(); name=Sc.nextLine(); a=Sc.nextInt(); if(a<=199) { b=a*1.20f; } else if(a>=200 && a<400) { b=a*1.50f; } else if (a>=400 && a<600) { b=a*1.80f; } else b=a*2.00f; if(b>400) { b=b*0.15f+b; } else b=b+100; System.out.format("%.2f",b); }}
