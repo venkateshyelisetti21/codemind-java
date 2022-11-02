b1db031c574cab0def791e9e1c9e35ad9943659c
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-02 18:40:36 +0530

    import java.util.Scanner; class Main { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int h; h=Sc.nextInt(); if(h<3) { System.out.print(h-1); } else { System.out.println(2); } }}

---
+++ The_Pandemic.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class Main
+{ public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int h; h=Sc.nextInt(); if(h<3) { System.out.print(h-1); } else { System.out.println(2); } }}
