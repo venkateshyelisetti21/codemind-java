3923e78238a801bcac022a59621b1a196d5fea14
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-03 19:38:10 +0530

    import java.util.Scanner; class sample { public static void moveZeroes(int x[],int n,int count) { for(int i=0;i<n;i++) { if(x[i]==0) count++; else { System.out.print(x[i]+" "); } } while(count-->0) { System.out.print(0+" "); } } public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int n,x[],count=0; n=Sc.nextInt(); x=new int[n]; for(int i=0;i<n;i++) { x[i]=Sc.nextInt(); } moveZeroes(x,n,count); }}

---
+++ Move_Zeroes.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class sample
+{ public static void moveZeroes(int x[],int n,int count) { for(int i=0;i<n;i++) { if(x[i]==0) count++; else { System.out.print(x[i]+" "); } } while(count-->0) { System.out.print(0+" "); } } public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int n,x[],count=0; n=Sc.nextInt(); x=new int[n]; for(int i=0;i<n;i++) { x[i]=Sc.nextInt(); } moveZeroes(x,n,count); }}
