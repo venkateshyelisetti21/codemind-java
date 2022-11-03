716d57faacb99912f3205f2cad05473dd67a48ae
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-03 19:26:47 +0530

    import java.util.Scanner; class Main { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int r; r=Sc.nextInt(); for(int i=1;i<=r;i++) { for(int j=1;j<=r;j++) { System.out.print(i+" "); } System.out.println(); } }}

---
+++ Pattern.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class Main
+{ public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int r; r=Sc.nextInt(); for(int i=1;i<=r;i++) { for(int j=1;j<=r;j++) { System.out.print(i+" "); } System.out.println(); } }}
