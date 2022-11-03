d97c334965be486c6138d8834253471d46421d2b
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-03 19:20:58 +0530

    import java.util.Scanner; class sample { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int rows=Sc.nextInt(); for(int i=1;i<=rows;i++) { for(int j=1;j<i;j++) { } { for(int j=i;j<=rows;j++) { System.out.print(j+" "); } System.out.println(); } } }}

---
+++ Print_the_following_pattern.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class sample
+{ public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int rows=Sc.nextInt(); for(int i=1;i<=rows;i++) { for(int j=1;j<i;j++) { } { for(int j=i;j<=rows;j++) { System.out.print(j+" "); } System.out.println(); } } }}
