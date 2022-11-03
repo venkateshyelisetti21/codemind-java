415649e1ade65135cfc562a2ab5b22c6e42bf01d
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-03 21:40:19 +0530

    import java.util.Scanner; class sample { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int C; C=Sc.nextInt(); float F=(float)(9*C)/5+32; System.out.format("%.2f",F); }}

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class sample
+{ public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int C; C=Sc.nextInt(); float F=(float)(9*C)/5+32; System.out.format("%.2f",F); }}
