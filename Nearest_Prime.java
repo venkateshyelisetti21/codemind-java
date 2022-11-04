f39f6355c7398633ae673d84955c98c5c51d909f
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-04 18:42:30 +0530

    import java.util.Scanner; class sample { public static boolean isprime(int n) { int count=0; for(int i=2;i<=(int)Math.sqrt(n);i++) { if(n%i==0) count++; } if(count==0) return true; else return false; } public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int t,n,i,d1,d2; t=Sc.nextInt(); while(t>0) { n=Sc.nextInt(); for(i=n;;i--) { if(isprime(i)) { d1=i; break; } } for(i=n;;i++) { if(isprime(i)) { d2=i; break; } } if((n-d1)<=(d2-n)) System.out.println(d1); else System.out.println(d2); } }}

---
+++ Nearest_Prime.java
@@ -0,0 +1,3 @@
+import java.util.Scanner;
+class sample
+{ public static boolean isprime(int n) { int count=0; for(int i=2;i<=(int)Math.sqrt(n);i++) { if(n%i==0) count++; } if(count==0) return true; else return false; } public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int t,n,i,d1,d2; t=Sc.nextInt(); while(t>0) { n=Sc.nextInt(); for(i=n;;i--) { if(isprime(i)) { d1=i; break; } } for(i=n;;i++) { if(isprime(i)) { d2=i; break; } } if((n-d1)<=(d2-n)) System.out.println(d1); else System.out.println(d2); } }}
